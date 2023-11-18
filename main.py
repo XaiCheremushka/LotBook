0a4fb23337b20c6f55c721f5d07bfd8052bdfde2
Ivan Dimitrov <vania55ru@mail.ru>
2023-11-19 01:11:10 +0600

    Добавление кнопки на команду admin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.executor import start_webhook
 
@@ -19,15 +19,27 @@ WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 WEBAPP_HOST = config.WEBAPP_HOST
 WEBAPP_PORT = config.WEBAPP_PORT
 
+
+
 async def on_startup(dispatcher):
     await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
     start.register_commands(dispatcher)
 
 
-
 async def on_shutdown(dispatcher):
     await bot.delete_webhook()
 
+
+@dp.message_handler(commands=['admin'])
+async def admin_panel(message: types.Message):
+    await message.answer("Выберите нужную команду", reply_markup=show_button(["Добавить книгу", "Добавить вопрос"]))
+
+
+def show_button(list_menu):
+    """Принимает список и превращает его в кнопки"""
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*list_menu)
+    return keyboard
 
 
 if __name__ == '__main__':
